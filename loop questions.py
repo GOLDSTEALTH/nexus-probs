80e9b88617fbb754e4ceb64423ac851534b52803
GOLDSTEALTH <syedaamairshareef@gmail.com>
2025-08-30 12:58:07 +0530

    _1_

---
+++ loop questions.py
@@ -0,0 +1,22 @@
+#question 1
+'''sentence = input("enter a sentence :")
+
+times = int(input('how many time to repeat it :'))
+
+for i in range(times):
+    print(sentence)
+
+#question 2
+
+n = int(input('enter a number :'))
+
+for i in range (1, n+1):
+    print(n)
+
+# question 3
+
+n = int(input('enter a number'))
+
+for i in range (n-1,1):
+    print n'''
+
